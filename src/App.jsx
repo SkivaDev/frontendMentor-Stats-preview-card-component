f9f4048e9201e47c0e4011c52bb8fd2cc314c7a9
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2023-08-26 08:17:34 -0500

    add labels for images, tailwind confing, styles text

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,56 @@
 import { useState } from "react";
 import "./App.css";
-import headerDesktop from './assets/images/image-header-desktop.jpg';
-import headerMobile from './assets/images/image-header-mobile.jpg';
+import headerDesktop from "./assets/images/image-header-desktop.jpg";
+import headerMobile from "./assets/images/image-header-mobile.jpg";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <main className="w-full h-screen flex justify-center items-center">
-      <div className="flex">
-        <div className="">
-          <h1 className="text-3xl font-bold">Get <mark>insights</mark> that help your business grow.</h1>
-          <p>
-            Discover the benefits of data analytics and make better decisions
-            regarding revenue, customer experience, and overall efficiency.
-          </p>
-
+      <div className="flex h-[400px] w-[1000px] gap-[20px]">
+        <div className="md:w-[41.4%] md:p-[35px] md:flex md:flex-col md:justify-between">
           <div>
+            <h1 className="text-3xl font-bold">
+              Get <mark>insights</mark> that help your business grow.
+            </h1>
+            <p className="text-sm text-trans_white">
+              Discover the benefits of data analytics and make better decisions
+              regarding revenue, customer experience, and overall efficiency.
+            </p>
+          </div>
+
+          <div className="w-full flex justify-between">
             <div>
-              <h2>10k+</h2>
-              <p>companies</p>
+              <h2 className="text-2xl font-bold">10k+</h2>
+              <p className="text-[11px] font-bold text-trans_white2 tracking-widest">
+                COMPANIES
+              </p>
             </div>
             <div>
-              <h2>314</h2>
-              <p>templates</p>
+              <h2 className="text-2xl font-bold">314</h2>
+              <p className="text-[11px] font-bold text-trans_white2 tracking-widest">
+                TEMPLATES
+              </p>
             </div>
             <div>
-              <h2>12m+</h2>
-              <p>queries</p>
+              <h2 className="text-2xl font-bold">12m+</h2>
+              <p className="text-[11px] font-bold text-trans_white2 tracking-widest">
+                QUERIES
+              </p>
             </div>
           </div>
         </div>
-        <div >
-
-        </div>
+        <picture className="md:w-[48.6%] lg:w-auto">
+          <source media="(min-width): 877px" srcSet={headerDesktop} />
+          <img
+            src={headerMobile}
+            alt="header-image"
+            className="filter-image h-full w-full object-cover"
+            width={540}
+            height={446}
+          />
+        </picture>
       </div>
     </main>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,29 @@ export default {
   ],
   theme: {
     extend: {
+      screens: {
+        md : "875px",
+        lg: "1080px",
+      },
+      lineHeight: {
+        paragraph: "1.67",
+        title: "1.22",
+        stats: "1.42",
+      },
+      backgroundColor: {
+        dark_blue: "hsl(233, 47%, 7%)",
+        desaturated_blue: "hsl(244, 38%, 16%)",
+        soft_violet: "hsl(277, 64%, 61%)",
+        trans_white: "hsla(0, 0%, 100%, 0.75)",
+        trans_white2: "hsla(0, 0%, 100%, 0.6)",
+      },
+      colors: {
+        dark_blue: "hsl(233, 47%, 7%)",
+        desaturated_blue: "hsl(244, 38%, 16%)",
+        soft_violet: "hsl(277, 64%, 61%)",
+        trans_white: "hsla(0, 0%, 100%, 0.75)",
+        trans_white2: "hsla(0, 0%, 100%, 0.6)",
+      },
       fontFamily: {
         inter: ["Inter", "sans-serif"],
         lexend: ["Lexend Deca", "sans-serif"],
